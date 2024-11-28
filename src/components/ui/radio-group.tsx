a13e7c8c86db89517ddf75a35d029cad49c02fb3
jose-donato <zmcdonato@gmail.com>
2024-11-28 13:23:47 +0000

    fix components structure

--- src/components/ui/radio-group.tsx
+++ src/components/ui/radio-group.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { cn } from "@/app/lib/utils";
+import { cn } from "@/lib/utils";
 import * as RadioGroupPrimitive from "@radix-ui/react-radio-group";
 import { DotIcon } from "lucide-react";
 import * as React from "react";
